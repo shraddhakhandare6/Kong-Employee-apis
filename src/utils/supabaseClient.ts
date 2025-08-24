fe0e5c70f1ba1841bc0749b84657148358fbd99d
shraddhakhandare6 <shraddhakhandare446@gmail.com>
2025-08-24 14:34:23 +0530

    update in code

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,18 @@
+// import { createClient } from '@supabase/supabase-js';
+// import 'dotenv/config';
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+// export const supabase = createClient(supabaseUrl, supabaseKey);
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL or Key is not defined in environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
